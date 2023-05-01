d9c3265182947ffeb1e7100643e0186085711d66
Yunus Emre <yunus1481emre@mail.com>
2023-05-01 11:40:54 +0300

    update

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -53,7 +53,7 @@ export default function ProductCarousel({ products }) {
 
   return (
     <Stack
-      maxWidth={isSm ? "335px" : isMobile ? "500px" : "650px"}
+      maxWidth={isSm ? "336px" : isMobile ? "496px" : "650px"}
       sx={{ position: "relative" }}
     >
       <Stack
